67bc0bf44a041af01f56d6010ad3bfd928976c92
kokomi simp :3 <beol0127@gmail.com>
2022-01-27 22:04:28 +0000

    chore: update whole map

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import time
 import os
+import random
 import getkey
 import getpass
 import cursor
+import socket
 from replit import db
 
 
@@ -45,10 +47,151 @@ grey_back = "\033[0;40m"
 
 def clear(): os.system("clear")
 
+replit_talk_map = [
+  "corner", 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  9, "p", "p", "p", "p", "p", "p", "p", "p", 99, 99, 99, 99, 99, 99, 99, 99, 0, 0, 0, 0, 10,
+  9, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "p", 99, 99, 99, 0, 0, 0, 0, 10,
+  9, 3, 0, "p", 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 0, 0, 0, 0, 10,
+  9, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "p", 99, 99, 99, 0, 0, 0, 0, 10,
+  9, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "p", "p", "p", 99, 99, 99, 99, 99, 10,
+  9, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, "p", 99, 99, 99, 99, 99, 99, 99, 99, 99, 10,
+  9, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "p", 99, 99, 99, 99, 99, 10,
+  9, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "p", 99, 99, 99, 99, 99, 99, 99, 99, 10,
+  9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "p", "p", "p", "p", 99, 99, 99, 99, 10,
+  9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "p", 0, 0, 0, 0, 0, 0, 99, 10,
+  9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "p", 0, 0, 0, 0, 0, 0, 99, 10,
+  9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "p", 0, 0, 0, 0, 0, 0, 99, 10,
+  9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "p", 0, 0, 0, 0, 0, 0, 99, 10,
+  9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "p", "p", "p", "p", 99, 99, 99, 99, 10, 
+  10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10
+]
+
+replit_home_map = [
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+]
+
+python_repl_map = [
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+]
+
+def print_map(themap):
+  _end = ""
+  i = 0
+  side_num = 0
+  letter = "a"
+  for x in themap:
+    i += 1
+    side_num += 1
+    if i == 22: 
+      _end = "\n"
+      i = 0
+      if side_num == 22:
+        letter = "b"
+      elif side_num == 44:
+        letter = "c"
+      elif side_num == 66:
+        letter = "d"
+      elif side_num == 88:
+        letter = "e"
+      elif side_num == 110:
+        letter = "f"
+      elif side_num == 132:
+        letter = "g"
+      elif side_num == 154:
+        letter = "h"
+      elif side_num == 176:
+        letter = "i"
+      elif side_num == 198:
+        letter = "j"
+      elif side_num == 220:
+        letter = "k"
+      elif side_num == 242:
+        letter = "l"
+      elif side_num == 264:
+        letter = "m"
+      elif side_num == 286:
+        letter = "n"
+      elif side_num == 308:
+        letter = "o"
+      elif side_num == 330:
+        letter = "p"
+    else: 
+      _end = ""
+    if themap == replit_talk_map:
+      if x == "corner":
+        print(" ", end = _end)
+      if x == 0:
+        print(" ", end = _end)
+      if x == 9:
+        print(letter, end = "")
+      if x == 1:
+        if side_num >= 10:
+          if side_num == 10:
+            print()
+        else:
+          print(side_num, end = _end)
+      elif x == 2:
+        print(white_back + blue + "All" + w, end = _end)
+      elif x == 3:
+        print(white_back + blue + "Announcements" + w, end = _end)
+      elif x == 4:
+        print(white_back + blue + "Ask" + w, end = _end)
+      elif x == 5:
+        print(white_back + blue + "Jam" + w, end = _end)
+      elif x == 6:
+        print(white_back + blue + "Tutorials" + w, end = _end)
+      elif x == 7:
+        print(white_back + blue + "Share" + w, end = _end)
+      elif x == 8:
+        print(white_back + blue + "Template" + w, end = _end)
+      elif x == "p":
+        print(white_back + "  " + w, end = _end)
+    
+
+def find_player() -> int:
+  index = -1
+  for i in map:
+    index += 1
+    if i == 2: 
+      return index
+
+# All global variables
 login_menu = True
 current_option_login = 1
+current_option_play = 1
 login_menu = True
 signup_menu = True
+
 
 cursor.hide()
 clear()
@@ -140,5 +283,59 @@ while True:
 
   signup_menu = False
   login_menu = False
+  play_menu = True
+  cursor.hide()
 
-  break
+  while play_menu:
+    print("ReplitTD")
+    if current_option_play == 1:
+      print(f"{yellow}> Quick play{w}")
+      print("Play online")
+      print("Competitive")
+    elif current_option_play == 2:
+      print("Quick play")
+      print(f"{yellow}> Play online{w}")
+      print("Competitive")
+    elif current_option_play == 3:
+      print("Quick play")
+      print("Play online")
+      print(f"{yellow}> Competitive{w}")
+    menu_i = getkey.getkey()
+    
+    if menu_i in [key_up, key_left, "w", "a"]:
+      a = current_option_play - 1
+      if a == 0:
+         current_option_play = 3
+      else:
+        current_option_play -= 1
+    elif menu_i in [key_down, key_right, "s", "d"]:
+      a = current_option_play + 1
+      if a == 4:
+        current_option_play = 1
+      else:
+        current_option_play += 1
+    elif menu_i in [space, "", "x"]:
+      play_menu = False
+      clear()
+    clear()
+  
+    cursor.show()
+
+  if current_option_play == 1:
+    print("Game setup")
+    map_lists = ["Replit Talk", "Python Repl", "Replit Home"]
+    map = random.choice(map_lists)
+
+    if map == map_lists[0]:
+      print_map(replit_talk_map)
+      input()
+    elif map == map_lists[1]:
+      print_map(python_repl_map)
+    elif map == map_lists[2]:
+      print_map(replit_home_map)
+
+  elif current_option_play == 2:
+    pass
+
+  elif current_option_play == 3:
+    pass
